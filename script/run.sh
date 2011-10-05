32f802bb9b95597f6e6e9cdbdb058d3f0546370d
Shinichiro Aska <s.aska.org@gmail.com>
2011-10-05 10:47:08 +0900

    fix path

--- script/run.sh
+++ script/run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-exec /home/aska/perl5/perlbrew/perls/perl-5.12.3/bin/start_server \
+exec /home/aska/perl5/perlbrew/perls/perl-5.14.2/bin/start_server \
 --port=5007 \
 --interval=3 \
---pid-file=/home/aska/Dropbox/product/7kai-Tasks/app.pid \
--- /home/aska/perl5/perlbrew/perls/perl-5.12.3/bin/plackup \
+--pid-file=/home/aska/github/7kai-Tasks/app.pid \
+-- /home/aska/perl5/perlbrew/perls/perl-5.14.2/bin/plackup \
 -E deployment \
 -s Starlet \
 --max-workers=20 \
--a /home/aska/Dropbox/product/7kai-Tasks/app.psgi
+-a /home/aska/github/7kai-Tasks/app.psgi
