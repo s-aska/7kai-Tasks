73dfda1c6e973cdb780d3cc9cb93a9a0a0279056
s-aska <s.aska.org@gmail.com>
2012-02-22 19:38:13 +0900

    fix fade off

--- htdocs/static/js/app-base-signin.js
+++ htdocs/static/js/app-base-signin.js
@@ -10,9 +10,9 @@ app.addListener('setup', function(){
         localStorage.removeItem('hash');
     }
     // $('.carousel').carousel();
-    setTimeout(function(){
-        $('header h1').animate({opacity: 1}, 3000);
-    }, 2000);
+    // setTimeout(function(){
+    //     $('header h1').animate({opacity: 1}, 3000);
+    // }, 2000);
     setTimeout(function(){
         $('header .container').animate({opacity: 1}, 3000);
     }, 2000);
