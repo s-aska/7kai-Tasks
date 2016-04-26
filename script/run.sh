96af6dd11746f1e17e931f4d6cf6bad8402bbeb4
aska <aska@linecorp.com>
2016-04-26 18:33:08 +0900

    Use 5000

--- script/run.sh
+++ script/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
 exec /home/aska/7kai-Tasks/local/bin/start_server \
---port=5007 \
+--port=5000 \
 --interval=3 \
 -- /home/aska/7kai-Tasks/local/bin/plackup \
 -E deployment \
