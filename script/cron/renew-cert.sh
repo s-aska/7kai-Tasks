69e6f22720e813110e3626be58d8eafe3819ad78
Shinichiro Aska <s.aska.org@gmail.com>
2016-06-11 16:25:51 +0900

    Add /etc/init.d/nginx reload

--- script/cron/renew-cert.sh
+++ script/cron/renew-cert.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# crontab
-# 0 4 8 * * /home/aska/tasks.7kai.org/scripts/renew-cert.sh
+# crontab (root)
+# 0 4 1 * * /home/aska/7kai-Tasks/script/renew-cert.sh
 
 /usr/local/src/letsencrypt/letsencrypt-auto certonly --webroot -d tasks.7kai.org --webroot-path /home/aska/7kai-Tasks/htdocs --renew-by-default
 /home/aska/tasks.7kai.org/scripts/ct-submit.sh
+/etc/init.d/nginx reload
