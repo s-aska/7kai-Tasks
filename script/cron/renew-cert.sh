3c512245679192a4244f1b8a70170018256edde2
aska <aska@linecorp.com>
2016-07-01 16:45:17 +0900

    Remove duplicate script

--- script/cron/renew-cert.sh
+++ script/cron/renew-cert.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # crontab (root)
-# 0 4 1 * * /home/aska/7kai-Tasks/script/renew-cert.sh
+# 0 4 1 * * /home/aska/7kai-Tasks/script/cron/renew-cert.sh
 
 /usr/local/src/letsencrypt/letsencrypt-auto certonly --webroot -d tasks.7kai.org --webroot-path /home/aska/7kai-Tasks/htdocs --renew-by-default
-/home/aska/tasks.7kai.org/scripts/ct-submit.sh
+/home/aska/7kai-Tasks/script/ct-submit.sh
 /etc/init.d/nginx reload
--- script/renew-cert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# crontab
-# 0 4 8 * * /home/aska/tasks.7kai.org/script/renew-cert.sh
-
-/usr/local/src/letsencrypt/letsencrypt-auto certonly --webroot -d tasks.7kai.org --webroot-path /home/aska/7kai-Tasks/htdocs --renew-by-default
-/home/aska/7kai-Tasks/script/ct-submit.sh
